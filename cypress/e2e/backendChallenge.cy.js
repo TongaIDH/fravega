159996732284d8697fbea566b70043c102028a04
TongaIDH <gastonfga@hotmail.com>
2024-11-13 14:38:37 -0300

    gastongarcia/both-get-TCs-are-complete

--- cypress/e2e/backendChallenge.cy.js
+++ cypress/e2e/backendChallenge.cy.js
@@ -2,24 +2,49 @@
 
 import { baseUrl, endpoints, payload } from "../support/fixtures/dataBack.json";
 
+
+
 describe("Backend Challenge", () => {
     // it("Should create a user", () => {
     //     cy.request({
     //         method: "POST",
-    //         url: baseUrl.concat(endpoints.postUser),
-    //         body: payload
+    //         url: "https://gorest.co.in/public/v2/users",
+    //         body: {
+    //             name: "Gaston Garcia",
+    //             email: "gaston@garcia.a",
+    //             gender: "male",
+    //             status: "active"
+    //         }
+    //     }).then((response) => {
+    //         expect(response.status).to.equal(201);
+    //         console.log(response.body)
+    //         expect(response.body).to.equal(object);
     //     })
     // });
-    it.only("Should obtain users", () => {
+
+
+    it("Should obtain users", () => {
         cy.request({
             method: "GET",
-            url: "https://gorest.co.in/public/v2/users"
+            url: `${baseUrl}${endpoints.getUsers}`
         }).then((response) => {
             expect(response.status).to.equal(200);
+            console.log(response.body)
             expect(response.body).to.not.be.empty;
+            console.log(response.body[0].id)
+            user = response.body[0].id
         })
     });
-    // it("Should obtain specific user", () => {
 
-    // });
+    let user = undefined;
+    it("Should obtain specific user", () => {
+        cy.request({
+                method: "GET",
+                url: `${baseUrl}${endpoints.getUsers}${user}`
+            }).then((response) => {
+                expect(response.status).to.equal(200);
+                console.log(response.body)
+                expect(response.body).to.not.be.empty;
+            })
+    });
 });
